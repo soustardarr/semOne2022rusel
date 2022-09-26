e7a10560478a9cc5dc1f6f8bb56ae07de8fdb6c1
Ruslan Kozlov <rus.lan0404@mail.ru>
2022-09-26 15:21:01 +0300

    занятие 26.09 2 вар

--- test_for_git/test_for_git/Program.cs
+++ test_for_git/test_for_git/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("hi brooooooooo");
+﻿
+double x = double.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+if ((y <= 1 && y >= 0 && Math.Abs(x) <= 1) && (Math.Abs(x) + Math.Abs(y) <= 1))
+{
+    Console.WriteLine("принадлежит");
+}
+else if (Math.Abs(x) <= 1 && ((y >= -1 * x * x) && (y < 0)))
+{
+    Console.WriteLine("принадлежит");
+
+}
+else
+{
+    Console.WriteLine("не принадлежит");
+}
+
